b9183c5bdca490603d273e62342dc1b45011eef3
ReKuShE <70316694+IRAQ-hacker@users.noreply.github.com>
2021-01-05 02:32:12 +0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,47 @@
+#colour section
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+#script coding starts
+clear
+echo " "
+echo " "
+echo '
+                  _  _  ___  ___   __  ____  ___ 
+                 ( )( )(  ,\(   \ (  )(_  _)(  _)
+                  )()(  ) _/ ) ) )/__\  )(   ) _)
+                  \__/ (_)  (___/(_)(_)(__) (___) 
+'|lolcat
+echo " "
+echo " "
+sleep 6.0
+clear
+echo -e "$red                         ChEcKiNg..>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg...>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg....>$rset"
+sleep 2.0
+clear
+echo -e "$red                         ChEcKiNg.....>$rset"
+sleep 2.0
+clear
+echo " "
+echo " "
+echo -e "$grn                updating the hack-instagram please wait$rset"
+sleep 2.0
+cd $HOME
+rm -rf FaceBook-Bot
+git clone https://github.com/iraq-hacker/FaceBook-Bot
+clear
+echo " "
+echo -e "$grn              Facebook Bot  has been updated check it now...$rset"
+sleep 3.0
+echo " "
+cd $HOME
+cd Facebook-Bot
+clear
+sleep 1.0
